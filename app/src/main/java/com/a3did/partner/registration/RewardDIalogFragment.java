caa0cecc4ff405cfadf983fb1db0dd934b1fbfe7
BeeBeeBig <boys1113@naver.com>
2016-11-27 18:43:05 +0900

    added DialogFragment for each system

---
+++ app/src/main/java/com/a3did/partner/registration/RewardDIalogFragment.java
@@ -0,0 +1,40 @@
+
+package com.a3did.partner.registration;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.a3did.partner.partner.R;
+
+/**
+ * Created by Joonhyun on 2016-11-27.
+ */
+
+public class RewardDIalogFragment extends DialogFragment{
+
+    public RewardDIalogFragment(){
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.fragment_account, null)).setPositiveButton("Regist",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
+
